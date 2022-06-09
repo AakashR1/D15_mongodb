e7fd32dd77e18237408a2762820550eb53d3bd3d
AakashR1 <105836903+AakashR1@users.noreply.github.com>
2022-06-09 15:52:04 +0530

    Delete src_mongoose directory

--- src_mongoose/DB/DBConnection.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-const ConnectDB = (url)=>{
-    mongoose.connect(url)
-}
-module.exports  = ConnectDB;
-
--- src_mongoose/routers/router.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const companyContoller = require('../controller/companyController');
-
-router.post('/addCompany',companyContoller.addCompany);
-router.get('/getCompany',companyContoller.getCompany);
-
-module.exports = router;
